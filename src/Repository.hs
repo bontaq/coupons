ea55452d7c7cf3142a976f7fd4943913742cf717
Ian <bontaq@gmail.com>
2021-05-16 21:56:10 -0400

    Got querying, inserting basics for jsonb

--- src/Repository.hs
+++ src/Repository.hs
@@ -1,32 +1,59 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 -- | Getting and adding coupons
 
 module Repository where
 
 import Models
 
+-- Database Stuff
+import Database.PostgreSQL.Simple
+import GHC.Generics
+import Data.Aeson hiding (Result)
+
 type StartDate = Integer
 type EndDate = Integer
 type MinAmount = Integer
 
-data RuleConstructor
-  = BigSpend  MinAmount         RuleConstructor
-  | DateRange StartDate EndDate RuleConstructor
-  | HasCoupon String            RuleConstructor
-  | HasItem   MinAmount Sku     RuleConstructor
-  | Locale    Currency          RuleConstructor
-  | Name      String -- nicely requires any ruleset to end in name
+data Expression
+  = BigSpend  MinAmount         Expression
+  | DateRange StartDate EndDate Expression
+  | HasCoupon String            Expression
+  | HasItem   MinAmount Sku     Expression
+  | Locale    Currency          Expression
+  -- Just for a bit of fun, allows expression of
+  -- OneOf [Locale "USA" (Name "US"), Locale "DE" (Name "Germany")] (Name "US-or-Germany")
+  | OneOf     [Expression]      Expression
+  -- nicely requires any ruleset to end in name
+  | Name      String
   deriving Show
 
 data Target
   = Slug String
   | WholeCart
 
-data RuleEffect
-  = AmountOff Integer Target
-  | PercentOff Integer Target
-  | FreeProduct Sku
+data Result
+  = AmountOff    Integer Target
+  | PercentOff   Integer Target
+  | FreeProduct  Sku
+  | ReferralUsed String -- eh why not
 
-data Rule = Rule RuleConstructor [RuleEffect]
+data Rule = Rule Expression [Result]
+
+-- query :: [Rule]
+query' = do
+  conn <- connectPostgreSQL "host=localhost dbname=coupon user=coupon password=password"
+  [Only i] <- query_ conn "select expression from rules" :: IO [Only Value]
+  print i
+
+-- insert :: Rule -> IO ()
+insert' = do
+  conn <- connectPostgreSQL "host=localhost dbname=coupon user=coupon password=password"
+  execute conn
+    "insert into rules (expression, result) values (?, ?)"
+    ("{}" :: String, "{}" :: String)
+  pure ()
 
 -- We can say:
 -- Rule
@@ -46,4 +73,5 @@ data Rule = Rule RuleConstructor [RuleEffect]
 --   [AmountOff 50 (Slug "shoes"), FreeProduct "mat"]
 
 -- getCouponsForSku :: Sku -> [Coupon]
+
 getCouponsForSku = undefined
