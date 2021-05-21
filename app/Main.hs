09970734294aae11746d2ded7b84f459f9fdad85
Ian <bontaq@gmail.com>
2021-05-21 02:14:04 -0400

    Crazy easy to add a DB pool

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,8 @@ module Main where
 import GHC.Generics
 -- DB
 import Database.PostgreSQL.Simple
+-- DB Pool
+import Data.Pool
 -- JSON handling
 import Data.Aeson hiding (json)
 -- Server
@@ -14,20 +16,40 @@ import Web.Scotty
 
 import Service
 
-newtype ErrResp = ErrResp
-  { message :: String
-  } deriving (Generic)
+
+-------------------
+-- Database pool --
+-------------------
+
+connectionInfo :: ConnectInfo
+connectionInfo =
+  defaultConnectInfo {
+      connectHost = "localhost"
+    , connectUser = "coupon"
+    , connectPassword = "password"
+    , connectDatabase = "coupon"
+  }
+
+startPool :: IO (Pool Connection)
+startPool = createPool (connect connectionInfo) close 1 10 10
+
+
+------------
+-- Server --
+------------
+
+newtype ErrResp = ErrResp { message :: String } deriving (Generic)
 
 instance ToJSON ErrResp where
 
-routes :: Connection -> ScottyM ()
-routes conn = do
+routes :: Pool Connection -> ScottyM ()
+routes pool = do
   post "/discounts" $ do
     -- gets the JSON from the post, turns it into our domain object
     (context :: Context) <- jsonData
 
     -- what it all comes down to: do any coupons apply?
-    actions <- runGetActions conn context
+    actions <- withResource pool (\conn -> runGetActions conn context)
 
     case actions of
       Left err -> json (ErrResp { message=show err })
@@ -36,8 +58,9 @@ routes conn = do
 main :: IO ()
 main = do
   -- here we could access the Env / read settings from a file / build a nice command line tool
-  conn <- connectPostgreSQL "host=localhost dbname=coupon user=coupon password=password"
+  pool <- startPool
+  -- conn <- connectPostgreSQL "host=localhost dbname=coupon user=coupon password=password"
 
   -- it expects a database connection, we should be passing in a pool instead of using the
   -- same connection
-  scotty 3000 (routes conn)
+  scotty 3000 (routes pool)
