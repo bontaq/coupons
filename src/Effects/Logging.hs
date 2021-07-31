90ae0d13061d537611b499b89588e0b0d4e93a38
Ian <bontaq@gmail.com>
2021-07-31 10:28:54 -0400

    There we go, good timestamp and logs are JSON

--- src/Effects/Logging.hs
+++ src/Effects/Logging.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor, KindSignatures, GADTs, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE OverloadedStrings, DuplicateRecordFields, DeriveGeneric, TypeApplications, DeriveFunctor, KindSignatures, GADTs, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Effects.Logging
   ( Log, log, runLogIO, runLogFile )
 where
@@ -12,6 +12,12 @@ import Control.Carrier.Writer.Strict
 import Control.Carrier.Lift
 import Control.Monad.IO.Class (MonadIO(..))
 import Data.Kind (Type)
+
+import Control.Applicative
+import Data.Aeson hiding (Error)
+import GHC.Generics hiding ((:+:))
+
+import Chronos
 
 -- Fast logger
 import System.Log.FastLogger
@@ -28,12 +34,12 @@ data Message
   | Warn  String
 
 -- Render a structured log message as a string.
-renderLogMessage :: Message -> String
+renderLogMessage :: Message -> LogMessage
 renderLogMessage message = case message of
-  Error message -> "[error] " ++ message
-  Debug message -> "[debug] " ++ message
-  Info  message -> "[info] "  ++ message
-  Warn  message -> "[warn] "  ++ message
+  Error message -> LogMessage { level="error", message=message }
+  Debug message -> LogMessage { level="debug", message=message }
+  Info  message -> LogMessage { level="info",  message=message }
+  Warn  message -> LogMessage { level="warn",  message=message }
 
 --
 -- The logging effect
@@ -43,7 +49,7 @@ renderLogMessage message = case message of
 -- the effect!
 
 data Log (m :: Type -> Type) k where
-  Write :: String -> Log m ()
+  Write :: LogMessage -> Log m ()
 
 log' :: Has Log sig m => Message -> m ()
 log' message = send (Write $ renderLogMessage message)
@@ -81,36 +87,74 @@ instance (MonadIO m, Algebra sig m) => Algebra (Log :+: sig) (LogIO m) where
     R other       -> LogIO (alg (runLogIO . handle) other context)
 
 
-data LogWrapper = LogWrapper
-
 newtype LogFileCarrier m a = LogFileCarrier (FastLogger -> m a)
   deriving (Functor)
 
-instance Functor m => Applicative (LogFileCarrier m) where
+instance Applicative m => Applicative (LogFileCarrier m) where
+  pure = LogFileCarrier . const . pure
+  LogFileCarrier f <*> LogFileCarrier a = LogFileCarrier (liftA2 (<*>) f a)
 
-instance Functor m => Monad (LogFileCarrier m) where
+instance Monad m => Monad (LogFileCarrier m) where
+  LogFileCarrier a >>= f = LogFileCarrier (\r -> a r >>= runLogFile r . f)
 
 runLogFile :: FastLogger -> LogFileCarrier m a -> m a
 runLogFile logger (LogFileCarrier runLogCarrier) = runLogCarrier logger
 
+data LogMessage = LogMessage
+  { level   :: String
+  , message :: String }
+  deriving (Generic, Show)
+
+instance ToJSON LogMessage where
+
+instance ToLogStr LogMessage where
+  toLogStr msg = toLogStr $ encode msg
+
+data LogMessageWithTimestamp = LogMessageWithTimestamp
+  { level     :: String
+  , message   :: String
+  , timestamp :: Datetime }
+  deriving (Generic, Show)
+
+instance ToJSON LogMessageWithTimestamp where
+
+instance ToLogStr LogMessageWithTimestamp where
+  toLogStr msg = toLogStr $ encode msg
+
+handleLogMessage :: Time -> LogMessage -> LogStr
+handleLogMessage ts logMessage =
+  let
+    LogMessage{ level=level, message=message } = logMessage
+  in
+    toLogStr $ LogMessageWithTimestamp
+      { level=level
+      , message=message
+      , timestamp=timeToDatetime ts
+      }
 
 instance (MonadIO m, Algebra  sig m) => Algebra (Log :+: sig) (LogFileCarrier m) where
   alg handle sig context = LogFileCarrier $ \logger -> case sig of
-    L (Write msg) -> context <$ liftIO (logger $ toLogStr msg)
+    L (Write msg) -> do
+      ts <- liftIO now
+      context <$ liftIO (logger $ handleLogMessage ts msg)
     R other       -> alg (runLogFile logger . handle) other context
 
 
 --
 -- Example usage
 --
-application :: Has Log sig m => m ()
-application = do
-  logInfo "hello"
 
-main :: IO ()
-main = do
-  (logger, cleanup) <- newFastLogger (LogStdout defaultBufSize)
-
-  runM
-    . runLogFile logger
-    $ application
+-- application :: Has Log sig m => m ()
+-- application = do
+--   logInfo "hello"
+--
+-- timeStampFormat :: TimeFormat
+-- timeStampFormat = "%Y-%m-%d %H:%M:%S"
+--
+-- main :: IO ()
+-- main = do
+--   (logger, cleanup) <- newFastLogger (LogStdout defaultBufSize)
+--
+--   runM
+--     . runLogFile logger
+--     $ application
